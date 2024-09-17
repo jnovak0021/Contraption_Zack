0ea10520c5042ce99a76141729d4e65b3187aff2
Jacob Novak <jnovak0021@pointloma.edu>
2024-09-17 11:00:13 -0700

    Jacob Novak - 09/17/2024 - File Parser class and creation of game objects ----- This commit contains the classes to load the files into the game. At the top level is a LoadLevel object which holds the entire game. It contains an ArrayList of Room Objects. Each room object contains basic info for each room such as the 2d tiles and the arrayList of GameObjects.
    In order to run this code, in main, run:
    
          LoadLevel ll = new LoadLevel();
    
          ll.readFile();
    
          ll.printGame(); //prints out boards

---
+++ LoadLevel.java
@@ -0,0 +1,257 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+/*
+ * Files are read in using the LoadLevel class
+ *    this LoadLevelClass is the main class and holds an array of GameRoom Objects
+ * each file (room) is stored in a GameRoom object
+ *    each Game room object contains:
+ *    a 2d arraylist that contains the tiles that are included in each level (i.e tile, wall, rope, or void)
+ *    an arraylist that contains the game Objects that are placed on the tile
+ */
+
+
+
+
+/*
+class to read in the contents of the 10 room levels stored in text files.
+The class stores each level in a gameRoom object which contains a 2d array with the tile locations 
+as well as an arraylist that contains the GameBbjects that are stored in each level
+*/
+public class LoadLevel
+{
+   //list of RoomObject 
+   ArrayList<RoomObject> rooms = new ArrayList<RoomObject>();
+   
+   //method to read in each room level
+   //for loop that calls privatereadFileMethod for each room in level 1
+   //returns a 3d arraylist of arraylist of gameObjects
+   public void readFile()
+   {
+     
+      //note -- this needs to be changed later when all levles exist
+      //loop through each of the 10 rooms 
+      for( int i = 0; i < 2; i++ )
+      {
+         //set index of arrayIn to return value of privateReadFile
+         System.out.println("reading in file " + i);
+         
+         rooms.add(privateReadFile("room" + i + ".txt"));
+      } 
+      
+
+   }
+   
+   //private readFile method to load each room  
+   //returns a RoomObject that contains the 2darray and the tiles 
+   private RoomObject privateReadFile(String roomFileName)
+   {
+      //RoomObject to return to list
+      RoomObject tempRoomObject = new RoomObject();
+         
+      int rows, columns;
+      
+      //temp 2d array of ints to hold values
+      int[][] temp2d;
+      
+      //temp ArrayList of GameObjects
+      ArrayList <GameObject> tempGameObjectArray = new ArrayList<GameObject>();
+         
+      try
+      {
+        
+         //read in file
+         Scanner scanner = new Scanner(new File(roomFileName));
+         
+         rows = scanner.nextInt();
+      
+         columns = scanner.nextInt();
+
+         temp2d = new int[rows][columns];
+ 
+
+         System.out.println(rows + " " + columns); 
+         for(int i = 0; i < rows; i++)
+         {
+            for(int j = 0; j < columns; j++)
+            {
+               temp2d[i][j] = scanner.nextInt();
+            }
+ 
+         }
+         
+         /*
+         CODE FOR GETTING THE PRINT FORMAT OF EACH FILE READ IN
+        
+         System.out.println("X: " + temp2d.length);
+         System.out.println("Y: " + temp2d[1].length);
+         for(int i = 0; i < temp2d.length; i++)
+         {
+            for(int j = 0; j < temp2d[i].length; j++)
+            {
+               System.out.print("[" + i + "][" + j + "]" + temp2d[i][j] + " ");
+            }
+            System.out.println();
+            //string to store the line of the file
+         }
+         */
+         
+         //read in the GameObject array at the end
+         while(scanner.hasNext())
+         {
+            //store the gameString
+            String gameStr = scanner.next();
+            
+            //loop over the gameString and call the constructor reading in each 
+            //<object><property><activated>:<startx>:<starty>:<endx>:<endy>
+            //GameObject g = new GameObject(gameStr.charAt(0), gameStr.charAt(1), gameStr.charAt(2), gameStr.charAt(3), gameStr.charAt(4), gameStr.charAt(5), gameStr.charAt(6));
+            //tempGameObjectArray.append(g);
+            tempGameObjectArray.add(new GameObject());
+            //System.out.println(gameStr.charAt(0) + " " + gameStr.charAt(1) + " " + gameStr.charAt(2) + " " +  gameStr.charAt(3) + " " + gameStr.charAt(4) + " " + gameStr.charAt(5) + " " + gameStr.charAt(6));
+         }
+         
+         //set member variables of Roomobject
+         tempRoomObject.setGameBoard2d(temp2d);
+         tempRoomObject.setGameObjectArray(tempGameObjectArray);
+         
+         //close scanner
+         scanner.close();
+         
+         
+      
+      }
+      catch( FileNotFoundException e)
+      {
+         System.out.println("File Not Found");
+      }
+      return tempRoomObject;
+      
+      
+   }
+   
+   public void printGame()
+   {
+      //call all RoomObjects print method
+      for(int i = 0; i < rooms.size(); i++)
+      {
+         System.out.println(rooms.get(i).toString());
+      }
+   }
+
+
+}
+
+//Objects that make up the game: place on top of the tiles
+//Place Holder Object
+class GameObject
+{
+   public String toString()
+   {
+      return "GameObject";
+   }
+}
+
+
+/*
+ * Class RoomObject -- stores the content read in by each individual file
+ * Member Variables:
+ *    contains a 2d array list of the game tiles
+ *    contains an arraylist of GameObjects that represnt things such as buttons...
+ * methods: 
+ *    accessor mutators for the 2d array and arraylist of GameObjects
+*/
+class RoomObject
+{
+   //2d array list of the game tiles
+   private int [][] gameBoard2d;
+   //arraylist of GameObjects that represnt things such as buttons...
+   private ArrayList<GameObject> gameObjectArray;
+
+//    public RoomObject(int[][] gameBoard2d, ArrayList<GameObject> gameObjectArray)
+//    {
+//       this.gameBoard2d = gameBoard2d;
+//       this.gameObjectArray = gameObjectArray;
+//    }
+
+   //accessors and mutator for gameBoard2dArray variables
+   public int[][] getGameBoard2d()
+   {
+      return gameBoard2d;
+   }
+   public void setGameBoard2d(int[][] gameBoard2d)
+   {
+      this.gameBoard2d = gameBoard2d;
+   }
+
+   //gameObject accesor, mutator
+   public ArrayList<GameObject> getGameObjectArray()
+   {
+      return gameObjectArray;
+   }
+   
+   //mutator
+   public void setGameObjectArray(ArrayList<GameObject> gameObjectArray)
+   {
+      this.gameObjectArray = gameObjectArray;
+   }
+   
+   //to string
+   public String toString()
+   {
+      String temp = "Room:\n";
+      temp += "tile\n";
+      for ( int i = 0; i < gameBoard2d.length; i++)
+      {
+         for (int j = 0; j < gameBoard2d[i].length; j++)
+         {
+            temp += gameBoard2d[i][j] + " ";
+         }
+         temp += "\n";
+      }
+      temp += "\n\n";
+      
+      //print gameObject array
+      for ( int i = 0; i < gameObjectArray.size(); i++ )
+      {
+         temp += gameObjectArray.get(i).toString() + " ";
+      }
+      temp += "\n";
+      return temp;
+   }
+}
+
+
+
+//Driver which is representative of the main driver file
+/*
+class MainClass
+{
+   public static void main(String[]args)
+   {
+      //LoadLevel obj
+      LoadLevel ll = new LoadLevel();
+      
+
+      //call readFile()
+      ll.readFile();
+      
+      ll.printGame();
+   }
+}
+*/
+
+
+
+
+//What I need from dorian Quimby 
+// talk about structure of Abstract GameObject Class
+//    Should have internal structure to refelect code:
+//                   //loop over the gameString and call the constructor reading in each 
+                     //<object><property><activated>:<startx>:<starty>:<endx>:<endy>
+//                   GameObject g = new GameObject(gameStr.charAt(0), gameStr.charAt(1), gameStr.charAt(2), gameStr.charAt(3), gameStr.charAt(4), gameStr.charAt(5), gameStr.charAt(6));
+//                   tempGameObjectArray.append(g);
+//                   System.out.println(gameStr.charAt(0) + " " + gameStr.charAt(1) + " " + gameStr.charAt(2) + " " +  gameStr.charAt(3) + " " + gameStr.charAt(4) + " " + gameStr.charAt(5) + " " + gameStr.charAt(6));
+// 
