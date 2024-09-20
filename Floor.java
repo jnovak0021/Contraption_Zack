76cb14e3edc53c01fabb71c9eae981b97f778524
Ethan Milner <emilner0021@pointloma.edu>
2024-09-20 01:15:35 +0000

    Ethan - Floor.java edited online with Bitbucket

--- Floor.java
+++ Floor.java
@@ -10,22 +10,39 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
-import javafx.scene.paint.Color;
 
 //Floors are tiles that Zack can walk on.
 //Their color depending on what the theme of the room is
 
+import javafx.scene.paint.Color;
+
 public class Floor extends Tile
 {
-    public Floor(int xPos, int yPos, Color myColor)
-    {
-        super(xPos, yPos, myColor); //call parent constructor
+   public Floor(int xPos, int yPos, Color myColor)
+   {
+      super(xPos, yPos, myColor); //call parent constructor
         
-    }
+   }
     
-    public String toString()
-    {
+   public String toString()
+   {
       return "Floor";
-    }
-    
-}
+   }   
+   public void drawMe(Pane pane) {
+    // Print position information
+     // System.out.print(" [x-" + getX() + " y-" + getY() + "]");
+   
+    // Create the main tile rectangle
+      Rectangle rectangle = new Rectangle((getX() * 100), (getY() * 100), 100, 100);
+      rectangle.setFill(getMyColor());
+   
+    // Create the border rectangle
+      Rectangle borderRectangle = new Rectangle((getX() * 100), (getY() * 100), 100, 100);
+      borderRectangle.setStroke(Color.DARKGRAY);
+      borderRectangle.setFill(Color.TRANSPARENT); // Make the border transparent
+      borderRectangle.setStrokeWidth(2); // Optional: set the border thickness
+   
+    // Add both rectangles to the pane
+      pane.getChildren().addAll(rectangle, borderRectangle);
+   }}
+
