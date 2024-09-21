f5e413e5a75c2e4fdfc00032be16bf1e88033fa1
Ethan Milner <emilner0021@pointloma.edu>
2024-09-21 01:42:48 +0000

    Ethan -Zack.java edited online with Bitbucket

--- Zack.java
+++ Zack.java
@@ -1,21 +1,43 @@
-import javafx.animation.AnimationTimer;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
 
-//Zack is a special gameobject that the player controls
-public class Zack extends GameObject
-{
-    public Zack(int xPos, int yPos, Color myColor)
-    {
-        super(xPos, yPos, myColor); //parent call
+// Zack is a special game object that the player controls
+public class Zack extends GameObject {
+    private Rectangle shape; // Shape representation
+
+    public Zack(int xPos, int yPos, Color myColor) {
+        super(xPos, yPos, myColor); // Parent constructor
+        shape = new Rectangle(xPos, yPos, 30, 30); // Example size for the visual representation
+        shape.setFill(myColor);
+    }
+
+    public Rectangle getShape() {
+        return shape;
+    }
+
+    public void updatePosition() {
+        // Update the rectangle's position based on GameObject's coordinates
+        shape.setX(getX());
+        shape.setY(getY());
+    }
+
+    // Getters for x and y positions
+    public int getXPos() {
+        return super.getX(); // Get x position from GameObject
+    }
+
+    public int getYPos() {
+        return super.getY(); // Get y position from GameObject
+    }
+
+    // Setters for x and y positions
+    public void setXPos(int newX) {
+        super.setX(newX); // Set x position in GameObject
+        updatePosition(); // Update the shape's position
+    }
+
+    public void setYPos(int newY) {
+        super.setY(newY); // Set y position in GameObject
+        updatePosition(); // Update the shape's position
     }
 }
