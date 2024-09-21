ae5e462d647acceba36e8d763887dfc2be4fd5f5
Ethan Milner <emilner0021@pointloma.edu>
2024-09-21 01:44:26 +0000

    Ethan - Tile.java edited online with Bitbucket

--- Tile.java
+++ Tile.java
@@ -10,27 +10,32 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
+import javafx.scene.paint.Color;
 
 //Tiles are the basic elementsthat compose the map.
 //Tile is still an abstract class because only children classes (i.e. floors, walls, etc) can be insantiated
 
 public abstract class Tile extends GameObject
 {
-    private boolean traverseable;
-    public Tile(int xPos, int yPos, Color myColor)
-    {
-        super(xPos, yPos, myColor); //call parent constructor
+   private boolean traverseable;
+   public Tile(int xPos, int yPos, Color myColor)
+   {
+      super(xPos, yPos, myColor); //call parent constructor
         
-    }
+   }
     
     //abstract methods for Tile
     //public abstract void draw();
     
-    public boolean isTraversable(){
-        return traverseable;
-    }
+   public boolean isTraversable(){
+      return traverseable;
+   }
     
-    public void setTraversable(boolean newValue){
-        traverseable = newValue;
-    }
+   public void setTraversable(boolean newValue){
+      traverseable = newValue;
+   }
+    
+   
+public abstract void drawMe(Pane pane);
+
 }
