248500a73ce4eaf5e19cec7208e0e6e92ea08cc4
Jacob Novak <jnovak0021@pointloma.edu>
2024-10-05 19:26:32 -0700

    Jacob - treadmill fully working - waiting on screw functionality to complete room

--- LoadLevel.java
+++ LoadLevel.java
@@ -314,6 +314,13 @@ public class LoadLevel
                //<object>:<property>:<activated>:<startx>:<starty>:<endx>:<endy>:<color>:<associativeNumber>
                tempMechanismArray.add(temp);
             }
+            else if(parts[0].equals("T"))
+            {
+               //<object>:<property>:<activated>:<startx>:<starty>:<endx>:<endy>:<color>:<associativeNumber>
+               temp = new Treadmill(parts[1], Boolean.parseBoolean(parts[2]),Integer.parseInt(parts[3]),Integer.parseInt(parts[4]),Integer.parseInt(parts[5]),Integer.parseInt(parts[6]), Color.web(parts[7]),Integer.parseInt(parts[8]), this);
+               //<object>:<property>:<activated>:<startx>:<starty>:<endx>:<endy>:<color>:<associativeNumber>
+               tempMechanismArray.add(temp);
+            }
          
             System.out.println(tempMechanismArray.get(tempMechanismArray.size()-1));
             System.out.println("adding mechanism to \t" + Integer.parseInt(parts[8]));
